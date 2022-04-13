34421d1e9c6a53142a765a0bc685a880fd5f0d28
Daniel Stephen <daniel.vergil2711@gmail.com>
2022-04-12 21:14:04 -0700

    Final Project

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 const Sequelize = require('sequelize');
-
-//allow for use of .env data
 require('dotenv').config();
 
-// create connection to db, whether local on on heroku using jawsdb
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
-    host: '127.0.1',
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
     dialect: 'mysql',
-    port: 3306
-  });
+    port: 3306,
+  }
+);
 
 module.exports = sequelize;
+
